c5c514d31c129b2ef6ef0d424ede1c4d1279f3f4
Aoife Pearson <aoifejo.pearson@googlemail.com>
2018-09-28 15:30:14 +0100

    created initial files

---
+++ RPSBot/SimpleBot.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BotInterface.Bot;
+using BotInterface.Game;
+
+namespace RPSBot
+{
+    class SimpleBot : IBot
+    {
+        public Move MakeMove(Gamestate gamestate)
+        {
+            return Move.R;
+        }
+    }
+}
