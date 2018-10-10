38320acdc3d759a4b7614ccdc212fa4cc934b1fd
APDigital <aoifejo.pearson@googlemail.com>
2018-10-10 10:18:23 +0100

    code to use the winning hand of the opposing players last move

--- RPSBot/SimpleBot.cs
+++ RPSBot/SimpleBot.cs
@@ -10,26 +10,58 @@ namespace RPSBot
 {
     public class SimpleBot : IBot
     {
+        public int DynamiteCount { get; set; }
         public Move MakeMove(Gamestate gamestate)
         {
-            Random random = new Random();
-            int randomnumber = random.Next(0, 3);
-
+            int random = GenerateRandomNumber(1, 4);
             Move move = Move.S;
-            switch (randomnumber)
-                {
-                    case 1:
-                       move = Move.R;
-                        break;
-                    case 2:
-                        move = Move.P;
-                        break;
-                    default:
-                        move = Move.S;
-                        break;
-                }
-            
+
+            if (CanPlayDynamite(gamestate) == true && random == 4)
+            {
+                move = Move.D;
+            }
+
+            switch (random)
+            {
+                case 1:
+                    move = Move.R;
+                    break;
+                case 2:
+                    move = Move.P;
+                    break;
+                default:
+                    move = Move.S;
+                    break;
+            }
+
             return move;
+        }
+
+        private bool CanPlayDynamite(Gamestate gamestate)
+        {
+            foreach (Round round in gamestate.GetRounds())
+            {
+                if (round.GetP1() == Move.D)
+                {
+                    DynamiteCount++;
+                }
+            }
+
+            if (DynamiteCount < 100)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public int GenerateRandomNumber(int fromInt, int toInt)
+        {
+            Random random = new Random();
+            int randomnumber = random.Next(fromInt, toInt);
+            return randomnumber;
         }
     }
 }
--- RPSBotTest/SimpleBotTest.cs
+++ RPSBotTest/SimpleBotTest.cs
@@ -15,10 +15,31 @@ namespace RPSBotTest
         public void BotPlaysRandomMove()
         {
             SimpleBot simplebot = new SimpleBot();
-            Enum move1 = simplebot.MakeMove(game);
-            Enum move2 = simplebot.MakeMove(game);
+            int random = simplebot.GenerateRandomNumber(0, 5);
 
-            Assert.AreNotEqual(move1, move2);
+            Assert.AreNotEqual(random, 6);
+        }
+
+        [TestMethod]
+        public void BotPlaysLessThan100Dynamites()
+        {
+            SimpleBot simplebot = new SimpleBot();
+
+
+            bool fail = true;
+            for (int i = 0; i < 1000; i++)
+            {
+                simplebot.MakeMove(game);
+            }
+            if (simplebot.DynamiteCount > 100)
+            {
+                fail = true;
+            }
+            else
+            {
+                fail = false;
+            }
+            Assert.IsFalse(fail);
         }
     }
 }
